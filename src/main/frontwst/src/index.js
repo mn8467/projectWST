a8d98e0ad4cc127bc0dbe45b3baaa404f821bfc8
mn8467 <mn8467@naver.com>
2024-06-12 17:02:05 +0900

    Router dom 적용

--- src/main/frontwst/src/index.js
+++ src/main/frontwst/src/index.js
@@ -7,7 +7,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+              <BrowserRouter>
+                 <Routes>
+                     <Route exact path="/" element={<App />}></Route>   {/*main col 12*/}
+                 </Routes>
+              </BrowserRouter>
   </React.StrictMode>
 );
 
