f45102ce58f35c9425b760bf5e4c03b6b15163ba
mn8467 <mn8467@naver.com>
2024-06-19 19:58:27 +0900

    front modifying

--- src/main/frontwst/src/Component/Home/Main.tsx
+++ src/main/frontwst/src/Component/Home/Main.tsx
@@ -132,33 +132,7 @@ export default function Main (){
                   <div className="grid grid-cols-4 gap-6 xl:grid-cols-1">
               
               
-                  {/* <!-- card --> */}
-                  <div className="report-card">
-                      <div className="card">
-                          <div className="card-body flex flex-col">
-                              
-                              {/* <!-- top --> */}
-                              <div className="flex flex-row justify-between items-center">
-                                  <div className="h6 text-indigo-700 fad fa-shopping-cart"></div>
-                                  <span className="rounded-full text-white badge bg-teal-400 text-xs">
-                                      12%
-                                      <i className="fal fa-chevron-up ml-1"></i>
-                                  </span>
-                              </div>
-                              {/* <!-- end top --> */}
               
-                              {/* <!-- bottom --> */}
-                              <div className="mt-8">
-                                  <h1 className="h5 num-4"></h1>
-                                  <p>items sales</p>
-                              </div>                
-                              {/* <!-- end bottom --> */}
-                  
-                          </div>
-                      </div>
-                      <div className="footer bg-white p-1 mx-4 border border-t-0 rounded rounded-t-none"></div>
-                  </div>
-             {/* end card */} 
               
               
                   {/* <!-- card --> */}
